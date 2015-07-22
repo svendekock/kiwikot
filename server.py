782045487a4acc8929caf5c13bde0827c1d27ce1
svendekock <sven@svendekock.be>
2015-07-22 14:11:26 +0200

    Added first try for controlling the motor

--- server.py
+++ server.py
@@ -15,11 +15,40 @@ import time
 root = os.path.dirname(__file__)
 port = 4567
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
+		# Variables
+
+		delay = 0.0055
+		steps = 500
+
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setwarnings(False)
+
+		# Enable GPIO pins for  ENA and ENB for stepper
+
+		enable_a = 18
+		enable_b = 22
+
+		# Enable pins for IN1-4 to control step sequence
+
+		coil_A_1_pin = 11
+		coil_A_2_pin = 13
+		coil_B_1_pin = 15
+		coil_B_2_pin = 16
+
+		# Set pin states
+
+		GPIO.setup(enable_a, GPIO.OUT)
+		GPIO.setup(enable_b, GPIO.OUT)
+		GPIO.setup(coil_A_1_pin, GPIO.OUT)
+		GPIO.setup(coil_A_2_pin, GPIO.OUT)
+		GPIO.setup(coil_B_1_pin, GPIO.OUT)
+		GPIO.setup(coil_B_2_pin, GPIO.OUT)
+
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(11, GPIO.OUT)
+# GPIO.setup(13, GPIO.OUT)
+# GPIO.setup(15, GPIO.OUT)
 
 
 class SocketHandler(websocket.WebSocketHandler):
@@ -43,6 +72,7 @@ class SocketHandler(websocket.WebSocketHandler):
 	def on_message(self, message):
 		print "Incoming ", message
 		self.write_message(message)
+		CONTROL()
 
 	def on_close(self):
 		self.connected = False
@@ -51,6 +81,44 @@ class SocketHandler(websocket.WebSocketHandler):
 		if self.connected:
 			print "in timeout looooop"
 			tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=1), self.timeout_loop)
+	
+	def CONTROL:
+
+		# Set ENA and ENB to high to enable stepper
+
+		GPIO.output(enable_a, True)
+		GPIO.output(enable_b, True)
+
+		# loop through step sequence based on number of steps
+
+		for i in range(0, steps):
+			setStep(1,0,1,0)
+			time.sleep(delay)
+			setStep(0,1,1,0)
+			time.sleep(delay)
+			setStep(0,1,0,1)
+			time.sleep(delay)
+			setStep(1,0,0,1)
+			time.sleep(delay)
+
+		# Reverse previous step sequence to reverse motor direction
+
+		for i in range(0, steps):
+			setStep(1,0,0,1)
+			time.sleep(delay)
+			setStep(0,1,0,1)
+			time.sleep(delay)
+			setStep(0,1,1,0)
+			time.sleep(delay)
+			setStep(1,0,1,0)
+			time.sleep(delay)
+		
+	# Function for step sequence
+	def setStep(w1, w2, w3, w4):
+	  GPIO.output(coil_A_1_pin, w1)
+	  GPIO.output(coil_A_2_pin, w2)
+	  GPIO.output(coil_B_1_pin, w3)
+	  GPIO.output(coil_B_2_pin, w4)
 
 class IndexHandler(web.RequestHandler):
     def get(self):
