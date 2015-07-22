5dc06f170f2151bac2b4255f716d6e74eabc794b
svendekock <sven@svendekock.be>
2015-07-22 14:17:41 +0200

    updated

--- server.py
+++ server.py
@@ -50,6 +50,42 @@ GPIO.setup(coil_B_2_pin, GPIO.OUT)
 # GPIO.setup(13, GPIO.OUT)
 # GPIO.setup(15, GPIO.OUT)
 
+def control():
+	# Set ENA and ENB to high to enable stepper
+
+	GPIO.output(enable_a, True)
+	GPIO.output(enable_b, True)
+
+	# loop through step sequence based on number of steps
+
+	for i in range(0, steps):
+		setStep(1,0,1,0)
+		time.sleep(delay)
+		setStep(0,1,1,0)
+		time.sleep(delay)
+		setStep(0,1,0,1)
+		time.sleep(delay)
+		setStep(1,0,0,1)
+		time.sleep(delay)
+
+	# Reverse previous step sequence to reverse motor direction
+
+	for i in range(0, steps):
+		setStep(1,0,0,1)
+		time.sleep(delay)
+		setStep(0,1,0,1)
+		time.sleep(delay)
+		setStep(0,1,1,0)
+		time.sleep(delay)
+		setStep(1,0,1,0)
+		time.sleep(delay)
+	
+# Function for step sequence
+def setStep(w1, w2, w3, w4):
+	GPIO.output(coil_A_1_pin, w1)
+	GPIO.output(coil_A_2_pin, w2)
+	GPIO.output(coil_B_1_pin, w3)
+	GPIO.output(coil_B_2_pin, w4)
 
 class SocketHandler(websocket.WebSocketHandler):
 #	@tornado.web.asynchronous
@@ -81,43 +117,7 @@ class SocketHandler(websocket.WebSocketHandler):
 		if self.connected:
 			print "in timeout looooop"
 			tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=1), self.timeout_loop)
-	
-	def control():
-		# Set ENA and ENB to high to enable stepper
 
-		GPIO.output(enable_a, True)
-		GPIO.output(enable_b, True)
-
-		# loop through step sequence based on number of steps
-
-		for i in range(0, steps):
-			setStep(1,0,1,0)
-			time.sleep(delay)
-			setStep(0,1,1,0)
-			time.sleep(delay)
-			setStep(0,1,0,1)
-			time.sleep(delay)
-			setStep(1,0,0,1)
-			time.sleep(delay)
-
-		# Reverse previous step sequence to reverse motor direction
-
-		for i in range(0, steps):
-			setStep(1,0,0,1)
-			time.sleep(delay)
-			setStep(0,1,0,1)
-			time.sleep(delay)
-			setStep(0,1,1,0)
-			time.sleep(delay)
-			setStep(1,0,1,0)
-			time.sleep(delay)
-		
-	# Function for step sequence
-	def setStep(w1, w2, w3, w4):
-		GPIO.output(coil_A_1_pin, w1)
-		GPIO.output(coil_A_2_pin, w2)
-		GPIO.output(coil_B_1_pin, w3)
-		GPIO.output(coil_B_2_pin, w4)
 
 class IndexHandler(web.RequestHandler):
     def get(self):
